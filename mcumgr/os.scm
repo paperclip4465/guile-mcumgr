5da53d6b5f985e92c905f55b789d79196b61a671
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2023-09-13 11:56:21 -0400

    Updated `mcumgr/os.scm` to make use of exceptions

--- mcumgr/os.scm
+++ mcumgr/os.scm
@@ -1,4 +1,5 @@
 (define-module (mcumgr os)
+  #:use-module (ice-9 match)
   #:use-module (mcumgr)
   #:use-module (cbor)
   #:export (os-group
@@ -7,30 +8,28 @@
 
 (define os-group 0)
 
-
 (define (echo smp-connection string)
   "Echo string over SMP-CONNECTION. Useful for testing connectivity.
 SMP-CONNECTION should be a thunk which accepts a single <smp-frame> argument."
-  (let* ((resp (smp-connection (smp-frame
-				(group os-group)
-				(command 0)
-				(op 0)
-				(data
-				 (scm->cbor `(("d" . ,string)))))))
-	 (data (cbor->scm (smp-data resp)))
-	 (rc (assoc-ref data "rc"))
-	 (r (assoc-ref data "r")))
-    (if rc
-	`((#f . ,(smp-error-from-code rc)))
-	r)))
+  (let ((resp (smp-connection (smp-frame
+			       (group os-group)
+			       (command 0)
+			       (op 0)
+			       (data
+				(scm->cbor `(("d" . ,string))))))))
+    (match (cbor->scm (smp-data resp))
+      ((("rc" . code))
+       (raise-exception (make-smp-exception (smp-error-from-code code))))
+      ((("r" . msg)) msg))))
 
 (define (reset smp-connection)
   "Reset device"
   (let* ((resp (smp-connection (smp-frame
 				(group os-group)
 				(command 5)
-				(op 2))))
-	 (data (cbor->scm (smp-data resp))))
-    (if (null? data)
-	`((rc . #:ok))
-	data)))
+				(op 2)))))
+    (match (cbor->scm (smp-data resp))
+      ((("rc" . code))
+       (raise-exception (make-smp-exception (smp-error-from-code code))))
+      (_
+       #t))))
